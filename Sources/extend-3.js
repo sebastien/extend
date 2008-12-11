9c8581e245d8d07217a46d0a1012f8b6aac2a830
Sebastien Pierre <sebastien@type-z.org>
2008-12-11 14:14:28 -0500

    Added Extend3 base

---
+++ Sources/extend-3.js
@@ -0,0 +1,22 @@
+var extend = extend || {}
+
+function _super( self ) {
+}
+
+function method( self ) {
+}
+
+function proxy( self ) {
+}
+
+function extend( self, parent ) {
+}
+
+function mix( self, parent ) {
+}
+
+function clone( self ) {
+}
+
+function instanciate( self ) {
+}
